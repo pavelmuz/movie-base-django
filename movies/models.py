231a1759715423373a3281cca108c8b67d5d0ed7
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-01-01 20:15:24 +0300

    Created and styled Feed View

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+    user_rating = models.FloatField()
+    user_review = models.TextField(max_length=500, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    poster_url = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    kinopoisk_url = models.CharField(max_length=300)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 
 def movies_view(request):
-    return render(request, 'movies/movies.html')
+    movies = Movie.objects.all().order_by('title')
+    context = {'movies': movies}
+    return render(request, 'movies/movies.html', context)
