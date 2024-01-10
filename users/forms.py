1935657b2561566cf61f8cc2e6f176c071bdb8ad
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-01-10 19:17:09 +0300

    Styled Edit account View

--- users/forms.py
+++ users/forms.py
@@ -21,12 +21,29 @@ class CustomUserCreationForm(UserCreationForm):
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ['name', 'username', 'birthday', 'profile_image', 'email']
+        fields = ['username', 'name', 'birthday', 'email', 'profile_image']
 
         labels = {
-            'name': 'Полное имя',
-            'username': 'Имя пользователя',
-            'birthday': 'Дата рождения',
-            'profile_image': 'Аватар',
-            'email': 'Электронная почта'
+            'name': 'Полное имя:',
+            'username': 'Имя пользователя:',
+            'birthday': 'Дата рождения:',
+            'profile_image': 'Фото профиля:',
+            'email': 'Электронная почта:'
         }
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control my-2',
+                'style': 'color: #0b666a;'
+            })
+
+        self.fields['email'].widget.attrs.update({
+            'type': 'email'
+        })
+
+        self.fields['profile_image'].widget.attrs.update({
+            'type': 'file'
+        })
