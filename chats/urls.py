fa2f00af245b220d30a37af118ad12e2a863b1cf
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-01-22 00:32:31 +0300

    Finished chat logic

--- chats/urls.py
+++ chats/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chats, name='chats')
+    path('', views.chats, name='chats'),
+    path('chat/<str:pk>/', views.chat, name='chat-details')
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from users.models import Profile
+from .models import Message
 
 
+@login_required(login_url='login')
 def chats(request):
-    context = {}
+    profile = request.user.profile
+    active_chats = Profile.objects.filter(
+        Q(sent_messages__recipient=profile) |
+        Q(recieved_messages__sender=profile)
+    ).distinct()
+    context = {
+        'active_chats': active_chats
+    }
     return render(request, 'chats/chats.html', context)
+
+
+@login_required(login_url='url')
+def chat(request, pk):
+    current_user = request.user.profile
+    recipient = Profile.objects.get(id=pk)
+    messages = Message.objects.filter(
+        (Q(sender=current_user) & Q(recipient=recipient)) |
+        (Q(sender=recipient) & Q(recipient=current_user))
+    ).order_by('timestamp')
+
+    if request.method == 'POST':
+        body = request.POST['chat-message']
+        Message.objects.create(
+            sender=current_user,
+            recipient=recipient,
+            body=body
+        )
+        return redirect('chat-details', pk)
+
+    context = {
+        'feed': messages
+    }
+    return render(request, 'chats/chat-detail.html', context)
