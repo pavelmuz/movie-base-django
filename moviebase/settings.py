bb980cecf348cbd16dc45923ca8b8dbb6cf823f3
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-02-18 00:53:57 +0300

    Changed JWT settings

--- moviebase/settings.py
+++ moviebase/settings.py
@@ -168,8 +168,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
