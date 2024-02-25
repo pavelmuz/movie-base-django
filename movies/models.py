488fa341a04b098cd15e5966aa75e982c0d230bf
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-02-25 19:48:03 +0300

    Changed Movie model field

--- movies/models.py
+++ movies/models.py
@@ -9,7 +9,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=200)
     user_rating = models.FloatField()
     user_review = models.TextField(max_length=500, blank=True, null=True)
-    description = models.TextField(max_length=500, blank=True, null=True)
+    description = models.TextField(max_length=1000, blank=True, null=True)
     poster_url = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     kinopoisk_url = models.CharField(max_length=300)
