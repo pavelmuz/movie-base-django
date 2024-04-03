b7640f6b6953dc99ae24ce86762d61027747908d
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-04-03 23:33:50 +0300

    Добавлен docker-compose

--- moviebase/settings.py
+++ moviebase/settings.py
@@ -283,6 +283,9 @@ CORS_ALLOWED_ORIGINS = [
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
